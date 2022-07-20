23336a5d71f9b96adacc18670122996334b387ae
KevinDelaFuente <kevin.delafuente@chicagobooth.edu>
2022-07-20 17:23:58 -0500

    create bird generations with random jumps

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import pygame
 from definitions import *
 from pipe import PipeCollection
-from bird import Bird
+from bird import BirdCollection
 
 def update_label(data, title, font, x, y, gameDisplay):
     label = font.render('{} {}'.format(title, data), 1, DATA_FONT_COLOR)
     gameDisplay.blit(label, (x, y))
     return y
 
-def update_data_labels(gameDisplay, dt, game_time, num_iterations, font):
+def update_data_labels(gameDisplay, dt, game_time, num_iterations, num_alive, font):
     y_pos = 10
     gap = 20
     x_pos = 10
     y_pos = update_label(round(1000/dt,2), 'FPS:', font, x_pos, y_pos + gap, gameDisplay)
     y_pos = update_label(round(game_time/1000,2),'Game time:', font, x_pos, y_pos + gap, gameDisplay)
     y_pos = update_label(num_iterations,'Iteration:', font, x_pos, y_pos + gap, gameDisplay)
+    y_pos = update_label(num_alive,'Alive:', font, x_pos, y_pos + gap, gameDisplay)
 
 def run_game():
     pygame.init()
@@ -26,7 +27,7 @@ def run_game():
     label_font = pygame.font.SysFont("monospace", DATA_FONT_SIZE)
     pipes = PipeCollection(gameDisplay)
     pipes.create_new_set()
-    robin = Bird(gameDisplay)
+    birds = BirdCollection(gameDisplay)
 
     clock = pygame.time.Clock()
     dt = 0
@@ -43,22 +44,23 @@ def run_game():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    robin.jump()
-                else:
-                    running = False
+                #uncomment to actually play and define a bird object
+                #if event.key == pygame.K_SPACE:
+                #    robin.jump()
+                #else:
+                running = False
 
         pipes.update(dt)
-        robin.update(dt, pipes.pipes)
+        num_alive = birds.update(dt, pipes.pipes)
 
-        if robin.state == BIRD_DEAD:
+        if num_alive == 0:
             pipes.create_new_set()
             game_time = 0
-            robin = Bird(gameDisplay)
+            birds.create_new_generation()
             num_iterations += 1
 
 
-        update_data_labels(gameDisplay, dt, game_time, num_iterations, label_font)
+        update_data_labels(gameDisplay, dt, game_time, num_iterations,num_alive, label_font)
         pygame.display.update()
 
 if __name__ == "__main__":
