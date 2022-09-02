666c7cdfc09a6ed5f9e186d76c1dec50521cf785
KevinDelaFuente <kevin.delafuente@chicagobooth.edu>
2022-09-02 00:27:49 -0500

    Added variation and offspring generation

--- bird.py
+++ bird.py
@@ -10,12 +10,11 @@ class Bird():
         self.state = BIRD_ALIVE
         self.img = pygame.image.load(BIRD_FILENAME)
         self.rect = self.img.get_rect()
-        self.set_position(BIRD_START_X,BIRD_START_Y)
         self.speed = 0
         self.fitness = 0
         self.time_lived = 0
         self.nnet = Nnet(NNET_INPUTS, NNET_HIDDEN, NNET_OUTPUTS)
-        # print("New bird")
+        self.set_position(BIRD_START_X,BIRD_START_Y)
 
     def reset(self):
         self.state = BIRD_ALIVE
@@ -25,15 +24,15 @@ class Bird():
         self.set_position(BIRD_START_X, BIRD_START_Y)
 
     def set_position(self, x, y):
-        self.rect.left =  x
-        self.rect.top = y
+        self.rect.centerx =  x
+        self.rect.centery = y
 
     def move(self, dt):
-        distance = 0 
+        distance = 0
         new_speed = 0
 
-        distance = self.speed * dt + 0.5*(GRAVITY* dt**2)
-        new_speed = self.speed + GRAVITY * dt
+        distance = (self.speed * dt) + 0.5*(GRAVITY* dt**2)
+        new_speed = self.speed + (GRAVITY * dt)
 
         self.rect.centery += distance
         self.speed = new_speed
@@ -54,9 +53,8 @@ class Bird():
     def check_status(self, pipes):
         if self.rect.bottom > DISPLAY_H:
             self.state = BIRD_DEAD
-            # print(self.time_lived)
         else:
-            self.check_hits(pipes) 
+            self.check_hits(pipes)
 
     def assign_collision_fitness(self, p):
         gap_y = 0
@@ -72,7 +70,6 @@ class Bird():
             if p.rect.colliderect(self.rect):
                 self.state = BIRD_DEAD
                 self.assign_collision_fitness(p)
-                # print(self.time_lived)
                 break
 
     def update(self, dt, pipes):
@@ -116,14 +113,14 @@ class BirdCollection():
 
     def create_new_generation(self):
         self.birds = []
-        for i in range(0,GENERATION_SIZE):
+        for i in range(0, GENERATION_SIZE):
             self.birds.append(Bird(self.gameDisplay))
 
     def update(self, dt, pipes):
         num_alive = 0
-        for j in self.birds:
-            j.update(dt, pipes)
-            if j.state == BIRD_ALIVE:
+        for b in self.birds:
+            b.update(dt, pipes)
+            if b.state == BIRD_ALIVE:
                 num_alive += 1
         return num_alive
 
@@ -132,13 +129,39 @@ class BirdCollection():
             b.fitness += b.time_lived * PIPE_SPEED
         self.birds.sort(key= lambda x: x.fitness, reverse= True)
 
-        for bird in self.birds[0:10]:
-            print('fitness', b.fitness)
+        #segment birds
+        cut_off = int(len(self.birds) * MUTATION_CUT_OFF)
+        good_birds = self.birds[0:cut_off]
+        bad_birds = self.birds[cut_off:]
+        bad_birds_to_take = int(len(self.birds) * MUTATION_BAD_TO_KEEP)
 
-        for bird in self.birds[2:-3]:
-            bird.nnet.modify_weights()
+        #Let's add variation to bad birds and append them in the next generation
+        for b in bad_birds:
+            b.nnet.modify_weights()
 
-        for bird in self.birds[-3:0]:
-            bird.nnet.create_mixed_weights(self.birds[0].nnet, self.birds[1].nnet)
+        new_birds = []
+        idx_bad_to_take = np.random.choice(np.arange(len(bad_birds)), bad_birds_to_take, replace=  False)
+        
+        for i in idx_bad_to_take:
+            new_birds.append(bad_birds[i])
 
-        self.create_new_generation()
+        #Let's add the good birds to the new generation
+
+        new_birds.extend(good_birds)
+
+        #Let's breed the good ones and add them to the new generation
+        birds_needed = len(self.birds) - len(new_birds)
+
+        while birds_needed > 0:
+            idx_to_breed = np.random.choice(np.arange(len(good_birds)), 2, replace=  False)
+            if idx_to_breed[0] != idx_to_breed[1]:
+                new_bird = Bird.create_offspring(good_birds[idx_to_breed[0]], good_birds[idx_to_breed[1]], self.gameDisplay)
+                if random.random() < MUTATION_MODIFY_CHANCE_LIMIT:
+                    new_bird.nnet.modify_weights()
+
+                new_birds.append(new_bird)
+
+        for b in new_birds:
+            b.reset()
+
+        self.birds = new_birds
--- nnet.py
+++ nnet.py
@@ -40,7 +40,7 @@ class Nnet:
         self.weight_hidden_output = Nnet.get_mix_from_arrays(net1.weight_hidden_output, net2.weight_hidden_output)
 
     def modify_array(a):
-        for x in np.nditer(a, op_flags= ['readwrite']):   
+        for x in np.nditer(a, op_flags=['readwrite']):   
             if random.random() < MUTATION_WEIGHT_MODIFY_CHANCE:
                 x[...] = np.random.random_sample() - 0.5
 
@@ -58,9 +58,9 @@ class Nnet:
             for col in range(0, num_cols):
                 index = row * num_cols + col
                 if index in idx:
-                    res[row,col] = ar1[row,col]
-                else: 
-                    res[row,col] = ar2[row,col]
+                    res[row][col] = ar1[row][col]
+                else:
+                    res[row][col] = ar2[row][col]
         
         return res
 
@@ -73,24 +73,24 @@ class Nnet:
 #     output = nnet.get_max_value(inputs)
 #     print('output', output, sep = '\n')
 
-def tests():
-    ar1 = np.random.uniform(-0.5, 0.5, size=(3, 4))
-    ar2 = np.random.uniform(-0.5, 0.5, size=(3, 4))
-    print('ar1.size', ar1.size, sep='\n')
-    print('original ar1:', ar1, sep='\n')
+# def tests():
+#     ar1 = np.random.uniform(-0.5, 0.5, size=(3, 4))
+#     ar2 = np.random.uniform(-0.5, 0.5, size=(3, 4))
+#     print('ar1.size', ar1.size, sep='\n')
+#     print('original ar1:', ar1, sep='\n')
 
-    Nnet.modify_array(ar1)
-    print('modified ar1:', ar1, sep='\n')
+#     Nnet.modify_array(ar1)
+#     print('modified ar1:', ar1, sep='\n')
 
-    print('')
+#     print('')
 
-    print('ar1', ar1, sep='\n')
-    print('ar2', ar2, sep='\n')
+#     print('ar1', ar1, sep='\n')
+#     print('ar2', ar2, sep='\n')
 
-    mixed = Nnet.get_mix_from_arrays(ar1, ar2)
-    print('mixed', mixed, sep='\n')
+#     mixed = Nnet.get_mix_from_arrays(ar1, ar2)
+#     print('mixed', mixed, sep='\n')
 
 
 
-if __name__ == "__main__":
-    tests()
+# if __name__ == "__main__":
+#     tests()
